b65ab7474575bc8ecc0ace70e2fbf3e7012d28a5
James Wilkinson <jwilkinson@James-Wilkinsons-MacBook-Pro.local>
2020-03-07 11:43:02 -0500

    Finished

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -4,5 +4,72 @@ package com.zipcodewilmington.looplabs;
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
-public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer>
+{
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications)
+    {
+        Integer[] output = new Integer[0];
+        for (Integer i = 0; i < array.length; i++)
+        {
+            Integer currentValue = array[i];
+            Integer occurrencesOfValue = 0;
+
+            for (Integer k = 0; k < array.length; k++)
+            {
+                if (array[i].equals(array[k]))
+                {
+                    occurrencesOfValue++;
+                }
+            }
+            if (occurrencesOfValue < maxNumberOfDuplications)
+            {
+                output = addToArray(currentValue, output);
+            }
+        }
+
+        return output;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Integer[] output = new Integer[0];
+        for (Integer i = 0; i < array.length; i++)
+        {
+            Integer currentValue = array[i];
+            Integer occurrencesOfValue = 0;
+
+            for (Integer k = 0; k < array.length; k++)
+            {
+                if (array[i].equals(array[k]))
+                {
+                    occurrencesOfValue++;
+                }
+            }
+            if (!occurrencesOfValue.equals(exactNumberOfDuplications))
+            {
+                output = addToArray(currentValue, output);
+            }
+        }
+
+        return output;
+    }
+
+    public Integer[] addToArray(Integer newValue, Integer[] oldArray)
+    {
+        Integer[] result = new Integer[oldArray.length + 1];
+
+        for(Integer i = 0; i < oldArray.length; i++)
+        {
+            result[i] = oldArray[i];
+        }
+
+        result[result.length - 1] = newValue;
+
+        return result;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,72 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray)
+    {
+        super(intArray);
+//        array = new String[intArray.length];
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications)
+    {
+        String[] output = new String[0];
+        for (Integer i = 0; i < array.length; i++)
+        {
+            String currentValue = array[i];
+            Integer occurrencesOfValue = 0;
+
+            for (Integer k = 0; k < array.length; k++)
+            {
+                if (array[i].equals(array[k]))
+                {
+                    occurrencesOfValue++;
+                }
+            }
+            if (occurrencesOfValue < maxNumberOfDuplications)
+            {
+                output = addToArray(currentValue, output);
+            }
+        }
+
+        return output;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        String[] output = new String[0];
+        for (Integer i = 0; i < array.length; i++)
+        {
+            String currentValue = array[i];
+            Integer occurrencesOfValue = 0;
+
+            for (Integer k = 0; k < array.length; k++)
+            {
+                if (array[i].equals(array[k]))
+                {
+                    occurrencesOfValue++;
+                }
+            }
+            if (!occurrencesOfValue.equals(exactNumberOfDuplications))
+            {
+                output = addToArray(currentValue, output);
+            }
+        }
+
+        return output;
+    }
+
+    public String[] addToArray(String newValue, String[] oldArray)
+    {
+        String[] result = new String[oldArray.length + 1];
+
+        for(Integer i = 0; i < oldArray.length; i++)
+        {
+            result[i] = oldArray[i];
+        }
+
+        result[result.length - 1] = newValue;
+
+        return result;
+    }
 }
